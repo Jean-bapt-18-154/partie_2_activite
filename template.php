12eb4f1abb49864868175af0f4ef99989a8e8a8f
Jean-bapt <jean-baptiste.courvoisier@gadz.org>
2017-10-22 13:34:41 +0200

    Created the template's html structure

---
+++ template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><? = $sTitle ?> </title>
+</head>
+<body>
+	<header>
+		
+	</header>
+	<div id="content">
+		<?= $htmlContent ?>
+	</div>
+	<footer>
+		
+	</footer>
+</body>
+</html>
